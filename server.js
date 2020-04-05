f16e6ebd22f5c10ed3bf7359115addb16952a7bc
jingks <w15148895221@163.com>
2020-04-05 17:58:02 +0800

    'init'

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 80;
+const public = 'dist';
+
+app.use(express.static(path.join(__dirname, public)));
+
+app.listen(port, () => {
+    console.log('App is listening at http://127.0.0.1:' + port);
+})
